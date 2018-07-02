872ddb8649c0c345ac156d44910a8f5cef816627
hanchaow94 <hanchaow94@gmail.com>
2018-07-02 15:47:22 -0700

    initial commit

--- service/main.go
+++ service/main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.227.111.167:9200/"
+	ES_URL = "http://35.196.160.196:9200/"
 )
 
 
