0793df8e52d006773d92207f1b814cbf1a3b46be
hanchaow94 <hanchaow94@gmail.com>
2018-07-23 19:36:18 -0700

    add around-web

---
+++ around-web/src/constants.js
@@ -0,0 +1,9 @@
+export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+export const TOKEN_KEY = 'TOKEN'; export const POS_KEY = 'POS';
+export const AUTH_PREFIX = 'Bearer';
+export const LOC_SHAKE = 0.02;
+export const GEO_OPTION = { 
+    enableHighAccuracy: true,
+    maximumAge: 3600000,
+    timeout: 27000,
+};
